18ecd9597c209690c427522d2c3f6e407a91b6e4
雷啸天 <canxuetian@outlook.com>
2021-08-08 10:37:50 +0800

    BlogNews&Type Controller

--- MyAspBlog.WebApi/Controllers/BlogNewsController.cs
+++ MyAspBlog.WebApi/Controllers/BlogNewsController.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using MyAspBlog.IService;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using MyAspBlog.Model;
+using MyAspBlog.WebApi.Utility.ApiResult;
 using System.Threading.Tasks;
 
 namespace MyAspBlog.WebApi.Controllers
@@ -18,10 +16,54 @@ namespace MyAspBlog.WebApi.Controllers
             this._iBlogNewsService = iBlogNewsService;
         }
         [HttpGet("BlogNews")]
-        public async Task<ActionResult> GetBlogNews()
+        public async Task<ActionResult<ApiResult>> GetBlogNews()
         {
-            var data =await _iBlogNewsService.QueryAsync();
-            return Ok(data);
+            var data = await _iBlogNewsService.QueryAsync();
+            if (data == null) return ApiResultHelper.Error("没有更多的文章");
+            return ApiResultHelper.Success(data);
+        }
+        /// <summary>
+        /// 添加文章
+        /// </summary>
+        [HttpPost("Create")]
+        public async Task<ActionResult<ApiResult>> Create(string title, string content, int typeId)
+        {
+            //数据验证。。。省略
+
+            BlogNews blogNews = new BlogNews
+            {
+                BrowseCount = 0,
+                Title = title,
+                Content = content,
+                LastModifiedTime = System.DateTime.Now,
+                LikeCount = 0,
+                TypeId = typeId,
+                AuthorId = 1,
+            };
+            bool b = await _iBlogNewsService.CreateAsync(blogNews);
+            if (!b) return ApiResultHelper.Error("创建失败，服务器发生错误");
+            return ApiResultHelper.Success(blogNews);
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<ActionResult<ApiResult>> Delete(int id)
+        {
+            bool b = await _iBlogNewsService.DeleteAsync(id);
+            if (!b) return ApiResultHelper.Error("删除，服务器发生错误");
+            return ApiResultHelper.Success(b);
+        }
+        [HttpPut("Edit")]
+        public async Task<ActionResult<ApiResult>> Edit(int id, string title, string content, int typeid)
+        {
+            BlogNews blogNews =await _iBlogNewsService.FindAsync(id);
+            if (blogNews == null) return ApiResultHelper.Error("未找到该文章");
+            blogNews.Title = title;
+            blogNews.Content = content;
+            blogNews.TypeId = typeid;
+            bool b =await _iBlogNewsService.EditAsync(blogNews);
+            if (!b) return ApiResultHelper.Error("修改失败，服务器错误");
+            return ApiResultHelper.Success(blogNews);
         }
     }
 }
+
---
+++ MyAspBlog.WebApi/Controllers/TypeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAspBlog.IService;
+using MyAspBlog.Model;
+using MyAspBlog.WebApi.Utility.ApiResult;
+using System.Threading.Tasks;
+
+namespace MyAspBlog.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TypeController : ControllerBase
+    {
+        private readonly ITypeInfoService _iTypeInfoService;
+        public TypeController(ITypeInfoService iTypeInfoService)
+        {
+            _iTypeInfoService = iTypeInfoService;
+        }
+        [HttpGet("Types")]
+        public async Task<ApiResult> Types()
+        {
+            var types = await _iTypeInfoService.QueryAsync();
+            if (types.Count == 0) return ApiResultHelper.Error("找不到更多类型");
+            return ApiResultHelper.Success(types);
+        }
+
+        [HttpPost("Create")]
+        public async Task<ApiResult> Create(string name)
+        {
+            #region 数据验证
+            if (string.IsNullOrWhiteSpace(name)) return ApiResultHelper.Error("类型名不能为空");
+            #endregion
+            TypeInfo type = new TypeInfo
+            {
+                Type = name
+            };
+            bool b = await _iTypeInfoService.CreateAsync(type);
+            if (!b) return ApiResultHelper.Error("创建类型失败");
+            return ApiResultHelper.Success(b);
+        }
+        [HttpPut("Edit")]
+        public async Task<ApiResult> Edit(int id, string name)
+        {
+            var type = await _iTypeInfoService.FindAsync(id);
+            type.Type = name;
+            bool b = await _iTypeInfoService.EditAsync(type);
+            if (!b) return ApiResultHelper.Error("修改失败");
+            return ApiResultHelper.Success(b);
+        }
+        [HttpDelete("Delete")]
+        public async Task<ApiResult> Delete(int id)
+        {
+            bool b = await _iTypeInfoService.DeleteAsync(id);
+            if (!b) return ApiResultHelper.Error("删除失败");
+            return ApiResultHelper.Success(b);
+        }
+    }
+}
